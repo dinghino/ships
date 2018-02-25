5b9c195ede7f3879d0f1234d3b609c9a8516f773
Daniele Ding <dinghino@gmail.com>
2018-02-25 16:59:18 +0100

    fuel tank comments, behaviours and stuff
    * added comments and documentation
    * added utility methods
    * added a new behaviour to the tank: can have infinite fuel
    * added a 'standard' implementation at the end for posterity

--- src/ships/parts/fuelTanks.ts
+++ src/ships/parts/fuelTanks.ts
@@ -9,30 +9,39 @@ export class FuelTank extends Part {
 
   private fuel: FuelState
 
-  constructor(manufacturer: string, vendor: string, public capacity: number) {
+  constructor(
+    /** who built the fuel tank */
+    manufacturer: string,
+    /** who sold the fuel tank */
+    vendor: string,
+    /** fuel tank maximum capacity */
+    public capacity: number
+  ) {
     super(manufacturer, vendor)
     this.fuel = FuelState.getFull(this)
   }
+
   /** Current fuel quantity in the tank */
   get currentFuel(): number { return this.fuel.quantity }
-  /** true if the tank has no fuel */
-  isEmpty() { return this.currentFuel === 0 }
+
+  isEmpty() { return !this.hasFuel() }
+  isFull() { return this.hasFuel(this.capacity) }
   /** Check that the tank has at least <qty> of fuel inside */
-  hasFuel(qty = 0): boolean { return this.currentFuel > qty }
+  hasFuel(qty = 1): boolean { return this.currentFuel >= qty }
 
   /**
    * add some fuel to the tank
    * @return Amount of fuel actuall added
-  */
-  refuel(amount: number): number {
+   */
+  addFuel(amount: number): number {
     return this.fuel.add(amount)
   }
   /**
    * request some fuel to the tank.
    * @return The actual quantity of fuel returned. Can be lower than what
    *         requested if the tank has less fuel
-  */
-  get(amount: number): number {
+   */
+  getFuel(amount: number): number {
     return this.fuel.remove(amount)
   }
 
@@ -82,8 +91,16 @@ export abstract class FuelState {
   static getEmpty(tank: FuelTank) {
     return new EmptyTank(tank, 0)
   }
+  static getinfinite(tank: FuelTank) {
+    return new InfiniteTank(tank, 100000) // the number does not matter since it won't be removed
+  }
 }
 
+/**
+ * The standard Tank behaviour, can be requested fuel - and will give it up to
+ * the available quantity - and will transition to an EmptyTank when the stored
+ * fuel ends.
+ */
 export class FullTank extends FuelState {
   remove(amount: number) {
     let removed = amount
@@ -105,12 +122,30 @@ export class FullTank extends FuelState {
   }
 }
 
+/**
+ * An Empty fuel tank, being empty, can only be filled with fuel and won't give
+ * back any fuel when requested. Will transition to a FullTank when some fuel
+ * is added (with the proper amount of fuel in it)
+ */
 export class EmptyTank extends FuelState {
   remove(amount: number) { return 0 }
   add(amount: number) {
     if (amount <= 0) return amount
 
     this.tank.changeState(FuelState.getFull(this.tank, amount))
+    return amount
+  }
+}
+
+/**
+ * An infinite fuel tank is able to handle any quantity of fuel and always
+ * returns the requested fuel
+ */
+export class InfiniteTank extends FuelState {
+  remove(amount: number): number {
+    return amount
+  }
+  add(amount: number): number {
     return amount
   }
 }
@@ -122,3 +157,42 @@ export const FuelTanks = {
 export enum TanksType {
   FUEL_TANK = 'Fuel Tank',
 }
+
+
+/**
+ * A 'Standard' implementation with the Proxy pattern whould look like following,
+ * but has the huge disadvantage that the behaviour of the tank cannot be changed
+ * with something different (like the InfiniteTank) since the actual implementation
+ * is embedded in the Tank itself and so it's static, so even though there is
+ * more code to handle, it's more dynamic and on the client it won't change
+ * anything.
+ *
+ * I Left out the actual Part super class implementation for simplicity but
+ * in an actual implementation it would be there
+ * ---------------------------------------------------------------------------
+ *
+ * export class ProxiedFuelTank {
+ *   private _fuelQty: number
+ * 
+ *   constructor(readonly capacity: number) {}
+ *
+ *   get fuelQuantity() { return this._fuelQty }
+ *   get isEmpty(): boolean { return !this.hasFuel() }
+ *   get isFull(): boolean { return this.hasFuel(this.capacity) }
+ *   hasFuel(quantity = 1) { return this._fuelQty >= quantity }
+ * 
+ *   getFuel(amount: number): number {
+ *     let output = amount
+ *     if (amount > this._fuelQty) output = this._fuelQty
+ *     this._fuelQty -= output
+ *     return output
+ *   }
+ *   addFuel(amount:number): number {
+ *     let input = amount
+ *     const space = this.capacity - this._fuelQty
+ *     if (amount > space) input = space
+ *     this._fuelQty += input
+ *     return amount - input
+ *   }
+ * }
+ */
